7e102adbf547b508e4805fc61d61b51725a0311a
unnikrishnan <unnikrishnan.b@landmarkgroup.in>
2019-12-04 11:22:16 +0530

    Initial commit
    Added all the required bundles for product service development
    - rest-bundle for API
    - easyadmin-bundle for admin interface for managing products
    - orm-pack for entities
    - api-doc-bundle for API documentation
    - serializer for serializing entity for API
    
    Also included other dev related packages

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,13 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
